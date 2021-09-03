40e568bac7a466c1abda29af90c0ce9be98a370c
Pedro Henrique Munhoz Costa <pedromunhozc@gmail.com>
2021-09-02 21:43:05 -0300

    Criado o AppLayout, usando conceitos do LinearLayout.
    - Aplicados também conceitos de ScrollView
    - Aplicada Localização em 2 linguagens
    - Criado arquivo de estilização retrato e paisagem
    
    Signed-off-by: Pedro Henrique Munhoz Costa <pedromunhozc@gmail.com>

---
+++ AppLayout/app/src/main/java/br/com/pedrohmunhoz/applayout/AgendaActivity.kt
@@ -0,0 +1,12 @@
+package br.com.pedrohmunhoz.applayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AgendaActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_agenda)
+    }
+}
