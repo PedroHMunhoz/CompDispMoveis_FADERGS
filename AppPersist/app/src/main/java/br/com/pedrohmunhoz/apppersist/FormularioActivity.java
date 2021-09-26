915faac840f42a864a3bcd973f9de0594aa44c1b
Pedro Henrique Munhoz Costa <pedromunhozc@gmail.com>
2021-09-26 17:44:51 -0300

    Implementa as classes e métodos para CRUD no SQLite.
    Adiciona chamada da tela de Formulário na Main.
    Adiciona validação e insert dos dados na tela do Formulário.
    Ajusta a leitura da lista de produtos para buscar do banco de dados na Main.
    
    Signed-off-by: Pedro Henrique Munhoz Costa <pedromunhozc@gmail.com>

--- AppPersist/app/src/main/java/br/com/pedrohmunhoz/apppersist/FormularioActivity.java
+++ AppPersist/app/src/main/java/br/com/pedrohmunhoz/apppersist/FormularioActivity.java
@@ -3,12 +3,68 @@ package br.com.pedrohmunhoz.apppersist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class FormularioActivity extends AppCompatActivity {
+
+    private EditText txtNomeProduto;
+    private Spinner cboCategorias;
+    private Button btnSalvar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
+
+        // Pegamos a referência do elemento da tela para o nosso objeto para manipular
+        txtNomeProduto = findViewById(R.id.txtNomeProduto);
+        cboCategorias = findViewById(R.id.cboCategorias);
+        btnSalvar = findViewById(R.id.btnSalvar);
+
+        // Criamos um clickListener para disparar quando o botão Salvar for clicado
+        btnSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Ao clicar, ele irá chamar o método Salvar que implementa a regra
+                salvar();
+            }
+        });
+    }
+
+    private void salvar() {
+        // Lemos do campo da tela o valor digitado pelo usuário
+        String nomeProduto = txtNomeProduto.getText().toString();
+
+        /** Se o nome estiver vazio ou a opção selecionada for 0, que é o "Selecione a categoria"
+         * vamos dar um Toast pro usuário preencher os dados completamente. O último parâmetro
+         * do Toast é uma constante de duração, sendo ele LONG ou SHORT.
+         */
+        if (nomeProduto.isEmpty() || cboCategorias.getSelectedItemPosition() == 0) {
+            Toast.makeText(this, "Você deve preencher todos os campos", Toast.LENGTH_LONG).show();
+        } else {
+            // Instanciamos um novo produto para popular as propriedades dele
+            Produto produto = new Produto();
+
+            // Seta o nome
+            produto.setNome(nomeProduto);
+
+            // Seta a categoria, pegando o texto que está na opção selecionada do Spinner
+            produto.setCategoria(cboCategorias.getSelectedItem().toString());
+
+            // Chama a classe de DAO passando o contexto e o produto a ser inserido no banco
+            ProdutoDAO.inserir(this, produto);
+
+            // Limpa o campo do nome do produto, para digitar um novo
+            txtNomeProduto.setText("");
+
+            // Seta o combo de Categorias para a primeira opção, com animação habilitada
+            cboCategorias.setSelection(0, true);
+
+            Toast.makeText(this, "Produto cadastro com sucesso!", Toast.LENGTH_LONG).show();
+        }
     }
 }
--- AppPersist/app/src/main/java/br/com/pedrohmunhoz/apppersist/MainActivity.java
+++ AppPersist/app/src/main/java/br/com/pedrohmunhoz/apppersist/MainActivity.java
@@ -1,5 +1,6 @@
 package br.com.pedrohmunhoz.apppersist;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -37,27 +38,47 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // Instancia uma nova intent, para navegar pra tela de cadastro
+                Intent intent = new Intent(MainActivity.this, FormularioActivity.class);
+
+                // Inicia a activity configurada
+                startActivity(intent);
             }
         });
     }
 
+    // Aqui fazemos override do método onRestart para que quando o usuário ir para a tela de
+    // cadastro e voltar, a lista seja atualizada novamente, e não somente ao abrir o programa
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+
+        // Chama o método para popular a lista e o adapter que será usado
+        carregarProdutos();
+    }
+
     private void carregarProdutos() {
-        lstProdutos = new ArrayList<>();
+        // Usando a classe ProdutoDAO, chamamos o método que vai buscar todos os produtos no banco
+        lstProdutos = ProdutoDAO.getProdutos(this);
 
-        Produto produto = new Produto("Coca Cola 1.5L", "Bebidas");
-        lstProdutos.add(produto);
+        // Se a lista não tiver nada, vamos criar um produto "fake" para mostrar na tela pro usuário
+        if (lstProdutos.size() == 0) {
+            // Produto placeholder, apenas para mostrar caso a lista venha vazia
+            Produto fake = new Produto("Lista vazia...", "");
+            lstProdutos.add(fake);
 
-        produto = new Produto("Pepsi 2L", "Bebidas");
-        lstProdutos.add(produto);
-
-        produto = new Produto("Trakinas", "Alimentos");
-        lstProdutos.add(produto);
+            // Desabilitamos as interações com a ListView, para que ela fique similar a uma label apenas
+            lvwProdutos.setEnabled(false);
+        } else {
+            // Habilitamos a interação com a ListView, caso a lista tenha produtos vindos do banco
+            lvwProdutos.setEnabled(true);
+        }
 
         /* O adapter recebe 3 parâmetros, sendo eles:
-        * Parâmetro 1: o contexto onde o mesmo será usado, nesse caso o "this" referencia a activity atual
-        * Parâmetro 2: o layout dos itens da lista
-        * Parâmetro 3: a lista de itens a serem exibidos
-        * * */
+         * Parâmetro 1: o contexto onde o mesmo será usado, nesse caso o "this" referencia a activity atual
+         * Parâmetro 2: o layout dos itens da lista
+         * Parâmetro 3: a lista de itens a serem exibidos
+         * * */
         adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, lstProdutos);
 
         // Seta o adapter na listview, para renderizar
