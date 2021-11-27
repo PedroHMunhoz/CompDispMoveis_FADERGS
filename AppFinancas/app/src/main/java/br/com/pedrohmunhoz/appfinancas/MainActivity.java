d3b163b1a0311b64bd0a70b8f45dca9e5b7dd475
Pedro Henrique Munhoz Costa <pedromunhozc@gmail.com>
2021-11-26 22:58:17 -0300

    Adiciona configuração inicial do Firebase.
    Signed-off-by: Pedro Henrique Munhoz Costa <pedromunhozc@gmail.com>

--- AppFinancas/app/src/main/java/br/com/pedrohmunhoz/appfinancas/MainActivity.java
+++ AppFinancas/app/src/main/java/br/com/pedrohmunhoz/appfinancas/MainActivity.java
@@ -2,13 +2,9 @@ package br.com.pedrohmunhoz.appfinancas;
 
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
