99f7e08604bbfd540bbc4bac6ea035a49ee23c44
KassandraPieretti <92534088+KassandraPieretti@users.noreply.github.com>
2021-12-02 18:45:25 -0300

    Tela Login
    Signed-off-by: KassandraPieretti <92534088+KassandraPieretti@users.noreply.github.com>

---
+++ AppFinancas/app/src/main/java/br/com/pedrohmunhoz/appfinancas/TelaInicial.java
@@ -0,0 +1,14 @@
+package br.com.pedrohmunhoz.appfinancas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TelaInicial extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_inicial);
+    }
+}
